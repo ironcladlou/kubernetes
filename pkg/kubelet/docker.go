19b02788d9e101d666b938e571b4d42c995aa9c2
Dan Mace <ironcladlou@gmail.com>
2014-07-24 16:54:44 +0000

    Remove redundant container list filter arg

--- pkg/kubelet/docker.go
+++ pkg/kubelet/docker.go
@@ -105,9 +105,9 @@ func (c DockerContainers) FindContainersByPodFullName(manifestID string) map[str
 }
 
 // GetKubeletDockerContainers returns a map of docker containers that we manage. The map key is the docker container ID
-func getKubeletDockerContainers(client DockerInterface, listAll bool) (DockerContainers, error) {
+func getKubeletDockerContainers(client DockerInterface) (DockerContainers, error) {
 	result := make(DockerContainers)
-	containers, err := client.ListContainers(docker.ListContainersOptions{All: listAll})
+	containers, err := client.ListContainers(docker.ListContainersOptions{All: true})
 	if err != nil {
 		return nil, err
 	}
